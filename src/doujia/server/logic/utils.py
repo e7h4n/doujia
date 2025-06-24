415ec47e99172a7a8c5e54dc62365a5d7d43dcf7
e7h4n <perfectworks@gmail.com>
2025-06-24 23:49:26 +0800

    fix: improve readability of time check in transaction comparison

--- src/doujia/server/logic/utils.py
+++ src/doujia/server/logic/utils.py
@@ -36,7 +36,12 @@ def get_existed_unique_no_set(main_file_path):
 
 
 def _compare_transactions(transaction: Transaction):
-    if "time" in transaction.meta and transaction.meta["time"] is not None and len(transaction.meta["time"]) > 0:
+    if (
+        transaction.meta is not None
+        and "time" in transaction.meta
+        and transaction.meta["time"] is not None
+        and len(transaction.meta["time"]) > 0
+    ):
         return datetime.combine(
             transaction.date,
             datetime.strptime(transaction.meta["time"], "%H:%M:%S").time(),
